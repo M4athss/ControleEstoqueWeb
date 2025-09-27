60a751fdbec8aa6c3515ab4e4ad06fd9dfc3594d
Matheus Benfica <matheus-augustob@hotmail.com>
2025-09-27 11:45:15 -0300

    criacao dos menus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-    $('#dropDown').click(function () {
+    // Seleciona todos os elementos cujo id começa com "dropDown"
+    $('[id^=dropDown]').click(function (e) {
+        e.stopPropagation();
+
+        // Fecha outros antes de abrir este
+        $('.drop-down').not($(this).closest('.drop-down')).removeClass('drop-down--active');
+        
         $(this).closest('.drop-down').toggleClass('drop-down--active');
     });
+
+    // Fecha se clicar fora
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.drop-down').length) {
+            $('.drop-down').removeClass('drop-down--active');
+        }
+    });
 });
+
